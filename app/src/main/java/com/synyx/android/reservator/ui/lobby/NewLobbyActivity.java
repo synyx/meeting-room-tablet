53a95fc34b439bc30931dc63c51ec23d8dbac46d
Max Dobler <dobler@synyx.de>
2018-11-14 22:40:13 +0100

    enable Fullscreen

--- app/src/main/java/com/synyx/android/reservator/ui/lobby/NewLobbyActivity.java
+++ app/src/main/java/com/synyx/android/reservator/ui/lobby/NewLobbyActivity.java
@@ -30,6 +30,7 @@ public class NewLobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        enableFullscreen();
         setContentView(R.layout.activity_newlobby);
 
         setRoomRecyclerView();
@@ -50,7 +51,7 @@ public class NewLobbyActivity extends AppCompatActivity {
     }
 
 
-   private void setClock() {
+    private void setClock() {
 
         TextView clockView = findViewById(R.id.clock);
         Date now = new Date();
@@ -63,7 +64,7 @@ public class NewLobbyActivity extends AppCompatActivity {
     private void enableFullscreen() {
 
         getWindow().getDecorView()
-            .setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE //
+            .setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY //
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE //
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN //
